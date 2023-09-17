67763215a0a62c8fb72e8159f13d93c0e15c6865
Andrii Prytula <a.s.pritula@gmail.com>
2023-09-18 00:40:17 +0300

    C#1

---
+++ src/main/java/com/softserveinc/ss_test_with_ai/SsTestWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ss_test_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsTestWithAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsTestWithAiApplication.class, args);
+    }
+
+}
